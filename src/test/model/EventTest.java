b48f6013a3f1ce4d01a882da9dea21320baa413e
Daniel Kaichis <56462051+danielkaichis@users.noreply.github.com>
2022-11-30 18:43:37 -0800

    coverage

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 // Code from AlarmSystem, https://github.students.cs.ubc.ca/CPSC210/AlarmSystem
 
@@ -36,5 +36,21 @@ public class EventTest {
     @Test
     public void testToString() {
         assertEquals(d.toString() + "\n" + "Player event", e.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        Event other = null;
+        String other2 = "String";
+        assertFalse(e.equals(other));
+        assertFalse(e.equals(other2));
+        assertTrue(e.equals(e));
+    }
+
+    @Test
+    public void testHashCode() {
+        int dateHash = e.getDate().hashCode();
+        int descHash = e.getDescription().hashCode();
+        assertEquals(13 * dateHash + descHash, e.hashCode());
     }
 }
